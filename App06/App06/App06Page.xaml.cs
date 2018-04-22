6d2e4737fa1c783e72eb2d6fc64735fdc216791b
Rogerio Fabricio <rogerio.fabricio@hotmail.com>
2018-04-22 08:57:42 -0300

    Teste Github
    teste github

--- App06/App06/App06Page.xaml.cs
+++ App06/App06/App06Page.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using Xamarin.Forms;
+using App06.Modelo;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Realms;
+using System.Linq;
 
 namespace App06
 {
@@ -7,6 +12,42 @@ namespace App06
         public App06Page()
         {
             InitializeComponent();
+
+            btnAdicionar.Clicked += delegate {
+
+                //Validar e Salvar
+                Produto produto = new Produto();
+                produto.Item = Item.Text;
+                produto.Quantidade = int.Parse(Quantidade.Text);
+
+                //Validação
+                List<ValidationResult> listRes = new List<ValidationResult>();
+                var contexto = new ValidationContext(produto);
+                bool valido = Validator.TryValidateObject(produto, contexto, listRes, true);
+
+                if (valido == false)
+                {
+                    string mensagem = string.Empty;
+
+                    foreach (var res in listRes)
+                    {
+                        mensagem += res.ErrorMessage + "\n";
+                    }
+
+                    DisplayAlert("Erros", mensagem, "OK");
+
+                } else {
+
+                    //Salvar
+                    var realm = Realm.GetInstance();
+
+                    realm.Write(() => {
+                        realm.Add(produto);
+                    });
+
+                    DisplayAlert("Salvo com sucesso!", "Itens no banco de dados: " + realm.All<Produto>().Count(), "OK");
+                }
+            };
         }
     }
 }
---
+++ App06/App06/Modelo/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Realms;
+
+namespace App06.Modelo
+{
+    public class Produto : RealmObject
+    {
+        [PrimaryKey]
+        public int Id { get; set;  }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Item { get; set;  }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Range(1, 999)]
+        public int? Quantidade { get; set;  }
+    }
+}
